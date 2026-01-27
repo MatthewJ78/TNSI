c8e28c0df3b13d62dca1cf27cec75f36ba47ee34
MatthewJ78 <matthew.juan@monlycee.net>
2026-01-27 13:10:06 +0000

    maj

---
+++ cours/commande.sql
@@ -0,0 +1,15 @@
+5 | chien | F | 2013-10-10 | Pepita | Bruyante |
+| F | 2012-08-23 | Lila
+| Peureuse
+7 | cheval | M | 2008-03-14 | Valentin | Rapide
+8 | cheval | F | 2008-03-14 | Valentine | Lente
+| 2019-10-01 | Nemo
+10 | oiseau | M | 2018-12-23 | Mozart
+|
+| Bruyant
+|
+|
+|
+|
+|
+11 | chien | F | 2015-02-01 | Windows | Gentille |
